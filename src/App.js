dbba0c2e601c6aeeb753934f6c770f4d5389484b
Kyle Richardson <kyle.richardson1@gmail.com>
2020-07-07 10:33:12 -0500

    basic setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>COVID info for me</h1>
+    </div>
+  );
+}
+
+export default App;
