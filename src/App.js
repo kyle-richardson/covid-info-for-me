b1ca03910f53ff69f1408318eeeccdb760fa24a3
Kyle Richardson <kyle.richardson1@gmail.com>
2020-07-07 11:18:59 -0500

    added county info search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import CountyInfo from "./components/CountyInfo";
 
 function App() {
   return (
     <div className="App">
       <h1>COVID info for me</h1>
+      <CountyInfo />
     </div>
   );
 }
---
+++ src/components/CountyInfo.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import TextField from "@material-ui/core/TextField";
+
+const CountyInfo = () => {
+  const [countyList, setCountyList] = useState([]);
+  const [myCountyString, setMyCountyString] = useState("Tarrant");
+  const [myCountyObject, setMyCountyObject] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_BASE_URL}/county`)
+      .then((res) => {
+        console.log(res);
+        const list = res.data.message;
+        setCountyList(list);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    if (myCountyString && countyList.length > 0) {
+      setMyCountyObject(
+        countyList.filter((ele) => ele.county_name === myCountyString)
+      );
+    }
+  }, [myCountyString, countyList]);
+
+  return (
+    <div className="county-info-container">
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <TextField
+          id="standard-basic"
+          label="Search County"
+          value={myCountyString}
+          onChange={(e) => setMyCountyString(e.target.value)}
+        />
+      </div>
+
+      {myCountyObject && <pre>{JSON.stringify(myCountyObject, null, 2)}</pre>}
+    </div>
+  );
+};
+
+export default CountyInfo;
